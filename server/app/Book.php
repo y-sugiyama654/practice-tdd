43f4494a7165ca781c09f5c0aa7df811b9b56565
Yuta <y.sugiyama54@gmail.com>
2020-01-29 00:51:59 +0900

    author_idにAuthor.idを設定

--- server/app/Book.php
+++ server/app/Book.php
@@ -13,4 +13,15 @@ class Book extends Model
     {
         return '/books/' . $this->id;
     }
+
+    /**
+     * Author.idをauthor_idに設定
+     * @param $author
+     */
+    public function setAuthorIdAttribute($author)
+    {
+        $this->attributes['author_id'] = (Author::firstOrCreate([
+            'name' => $author
+        ]))->id;
+    }
 }
