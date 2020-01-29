e258f54669c3b5e27e8cbe8d7d2305cc251ffc81
Yuta <y.sugiyama54@gmail.com>
2020-01-29 23:25:16 +0900

    インデントの修正

--- server/tests/Unit/BookReservationTest.php
+++ server/tests/Unit/BookReservationTest.php
@@ -14,15 +14,15 @@ class BookReservationTest extends TestCase
 
     /** @test */
     public function a_book_can_be_checked_out()
-{
-$book = factory(Book::class)->create();
-$user = factory(User::class)->create();
+    {
+        $book = factory(Book::class)->create();
+        $user = factory(User::class)->create();
 
-$book->checkout($user);
+        $book->checkout($user);
 
-$this->assertCount(1, Reservation::all());
-$this->assertEquals($user->id, Reservation::first()->user_id);
-$this->assertEquals($book->id, Reservation::first()->book_id);
-$this->assertEquals(now(), Reservation::first()->checked_out_at);
-}
+        $this->assertCount(1, Reservation::all());
+        $this->assertEquals($user->id, Reservation::first()->user_id);
+        $this->assertEquals($book->id, Reservation::first()->book_id);
+        $this->assertEquals(now(), Reservation::first()->checked_out_at);
+    }
 }
