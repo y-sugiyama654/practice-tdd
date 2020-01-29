331c5c4ad6405b646340720eff511b8d3d6d0dbb
Yuta <y.sugiyama54@gmail.com>
2020-01-29 23:57:11 +0900

    同じ本を2回借りることができることを確かめるテストコードの作成

--- server/app/Book.php
+++ server/app/Book.php
@@ -20,7 +20,22 @@ class Book extends Model
             'user_id' => $user->id,
             'checked_out_at' => now(),
         ]);
+    }
 
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if (is_null($reservation)) {
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now(),
+        ]);
     }
 
     /**
